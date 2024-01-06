04b2576fdf494b4b26ddf32ab25112e7ff80acdf
Shunya Fujigaki <wisteria.shunya@gmail.com>
2024-01-06 14:03:36 +0000

    Add : head settings

--- front/app/nuxt3weblog/nuxt.config.ts
+++ front/app/nuxt3weblog/nuxt.config.ts
@@ -58,6 +58,36 @@ export default defineNuxtConfig({
     }
   },
 
+  app:{
+    head:{
+      titleTemplate: '%s - ' + process.env.SITE_TITLE,
+      title: process.env.SITE_TITLE,
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.META_DESCRIPTION },
+  
+        { hid: 'og:site_name', property: 'og:site_name', content: process.env.SITE_TITLE },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: process.env.META_OG_URL },
+        { hid: 'og:title', property: 'og:title', content: process.env.SITE_TITLE },
+        { hid: 'og:description', property: 'og:description', content: process.env.META_DESCRIPTION },
+        { hid: 'og:image', property: 'og:image', content: process.env.META_OG_IMG },
+        { hid: 'twitter:card', property: 'twitter:card', content: 'summary' },
+        { hid: 'twitter:site', property: 'twitter:site', content: process.env.META_TWITTER_ID }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/favicon.ico' }
+      ]
+    },
+  
+  },
+
   devtools: { enabled: true },
   modules: [
     'vuetify-nuxt-module',
