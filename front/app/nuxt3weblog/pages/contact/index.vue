9d2caf529de1e33ed9096cee6327e733d42602c9
Shunya Fujigaki <wisteria.shunya@gmail.com>
2024-01-08 05:12:55 +0000

    Add : contact page

---
+++ front/app/nuxt3weblog/pages/contact/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="post">
+      <h1>Contact</h1>
+
+      <div style="margin:20px 0px;">
+          <p class="textarea">当サイトへのお問い合わせはこちらからどうぞ。</p>
+      </div>
+
+      <v-form style="margin:30px 0px;" name="contact" method="POST" action="/contact/success/" data-netlify="true">
+          <v-text-field
+              label="お名前"
+              required
+              color="blue-grey lighten-1"
+              name="name"
+          ></v-text-field>
+          <v-text-field
+              label="メールアドレス"
+              required
+              color="blue-grey lighten-1"
+              name="email"
+          ></v-text-field>
+          <v-text-field
+              label="件名"
+              color="blue-grey lighten-1"
+              name="title"
+          ></v-text-field>
+          <v-textarea
+              label="本文"
+              color="blue-grey lighten-1"
+              name="body"
+          ></v-textarea>
+        <!-- <v-btn depressed large color="grey lighten-1" width="100%" type="submit">送信</v-btn> -->
+        <v-btn depressed large color="grey-lighten-2" width="100%" type="submit">送信</v-btn>
+      </v-form>
+      
+      <v-breadcrumbs :items="breadcrumbs" style="padding:30px 5px 30px 5px;"></v-breadcrumbs>
+  </section>
+</template>
+
+<script setup>
+const breadcrumbs = [
+  {
+    title: "Home",
+    disabled: false,
+    href: "/"
+  },
+  {
+    title: "Contact",
+    disabled: true,
+    href: ""
+  },
+]
+
+useHead({
+  title:"Contact"
+})
+</script>
---
+++ front/app/nuxt3weblog/pages/contact/success.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="post">
+      <h1>Thank you</h1>
+      
+      <v-breadcrumbs :items="breadcrumbs" style="padding:30px 5px 30px 5px;"></v-breadcrumbs>
+  </section>
+</template>
+
+<script setup>
+const breadcrumbs = [
+  {
+    title: "Home",
+    disabled: false,
+    href: "/"
+  },
+  {
+    title: "Contact",
+    disabled: true,
+    href: ""
+  },
+]
+
+useHead({
+  title:"Contact"
+})
+</script>
