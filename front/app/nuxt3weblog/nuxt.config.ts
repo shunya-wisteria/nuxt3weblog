ddb8303c71d9a418fd5391f98a1535c2065fcd78
Shunya Fujigaki <wisteria.shunya@gmail.com>
2024-01-08 12:17:15 +0000

    Add : draft page

--- front/app/nuxt3weblog/nuxt.config.ts
+++ front/app/nuxt3weblog/nuxt.config.ts
@@ -147,6 +147,7 @@ export default defineNuxtConfig({
   microCMS: {
     serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
     apiKey: process.env.MICROCMS_API_KEY,
+    target: "all",
   },
 
   plugins: [
---
+++ front/app/nuxt3weblog/pages/draft/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <template v-if="data">
+    <Post :post="(data)"></Post>
+  </template>
+</template>
+
+<script setup>
+const { query } = useRoute();
+const config = useRuntimeConfig();
+
+const postId = query.id;
+const draftKey = query.draftKey;
+
+const { data } = await useMicroCMSGetListDetail({
+  endpoint: "posts",
+  contentId: postId,
+  queries: {draftKey:draftKey}
+});
+
+</script>
